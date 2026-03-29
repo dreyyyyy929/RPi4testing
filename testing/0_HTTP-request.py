0e329e85e9f076095a442cb09cb24d3a8af22523
dreyyyyy929 <audreyhytang929@gmail.com>
2026-03-29 13:29:05 +0800

    remove files

--- testing/0_HTTP-request.py
+++
@@ -1,4 +0,0 @@
-import requests
-url='https://trusted-pony-deadly.ngrok-free.app/predict'
-results = requests.get(url)
-print(results.text)
--- testing/1_LED-blink.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import LED
-from time import sleep
-led = LED(23)
-while True:
-    led.on()
-    sleep(0.3)
-    led.off()
-    sleep(0.3)
